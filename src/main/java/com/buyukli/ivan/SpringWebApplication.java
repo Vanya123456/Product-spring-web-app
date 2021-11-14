cf3e897293edf67914298f0a09b5caaf38561167
Ivan <ivan.buyukli21@gmail.com>
2021-11-14 13:22:34 +0100

    Name refactoring. Application to HW 3 is finished

--- src/main/java/com/buyukli/ivan/SpringWebApplication.java
+++ src/main/java/com/buyukli/ivan/SpringWebApplication.java
@@ -1,4 +1,4 @@
-package com.geekbrains.spring.web;
+package com.buyukli.ivan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
